7d1f1aebbd489d2cd163cc02682915b0a1fe39be
Tasos Chytiris <comblmn@gmail.com>
2025-05-14 19:37:53 +0300

    implemented TemporalBarrierStats

--- src/data/scripts/shipsystems/TemporalBarrierStats.java
+++ src/data/scripts/shipsystems/TemporalBarrierStats.java
@@ -5,119 +5,94 @@ import com.fs.starfarer.api.combat.*;
 import com.fs.starfarer.api.combat.listeners.AdvanceableListener;
 import com.fs.starfarer.api.combat.listeners.DamageTakenModifier;
 import com.fs.starfarer.api.impl.combat.BaseShipSystemScript;
-import data.scripts.hullmods.LakhesisGuardTimeflow;
 import org.lwjgl.util.vector.Vector2f;
 
-import java.util.ArrayList;
-import java.util.Map;
 
 public class TemporalBarrierStats extends BaseShipSystemScript {
 
-	public static float DAMAGE_MULT = 99f;
-	//public static float DAMAGE_MULT = 0.8f;
-	private Float mass = null;
-	
-	public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
-		float Mult = DAMAGE_MULT;
-		Map<String, Object> customCombatData = Global.getCombatEngine().getCustomData();
+	public static final float DAMAGE_MULT = 99f;
+	private static final float DURATION = 5f;
+	private static final float DAMAGE_FOR_MAX_BONUS = 8000f;
+	private static final float DAMAGE_BONUS_STEP = 400f;
+	private static final float BONUS_MULT_STEP = 2.5f;
+	private static final float DAMAGE_BONUS_EXTRA_MULT = 1.5f;
+	private static final float HIT_BONUS_EXTRA_MULT = 0.7f;
 
-		ShipAPI ship = null;
+	private Float originalMass = null;
+	private static State previousState = State.IDLE;
+	private static float lastActivationTime = 0f;
+
+	private SystemDamageTakenBuffListener listener;
+
+	public void apply(MutableShipStatsAPI stats, String id, State state, float effectLevel) {
+		ShipAPI ship;
 		if (stats.getEntity() instanceof ShipAPI) {
 			ship = (ShipAPI) stats.getEntity();
 		} else {
 			return;
 		}
-		if(mass == null){
-			mass = ship.getMass();
+
+
+		if (!ship.hasListenerOfClass(SystemDamageTakenBuffListener.class)) {
+			listener = new SystemDamageTakenBuffListener();
+			ship.addListener(listener);
 		}
-		if(state == state.OUT){
-			ship.setMass(mass);
+
+		if (previousState == State.IDLE && state == State.IN){
+			lastActivationTime = Global.getCombatEngine().getTotalElapsedTime(false);
 		}
-		else{
-			if (ship.getMass() == mass){
-				ship.setMass(mass * 10f);
+
+		if(previousState != state ){
+			previousState = state;
+		}
+
+		if(originalMass == null){
+			originalMass = ship.getMass();
+		}
+
+		if(state == State.OUT){
+			ship.setMass(originalMass);
+		} else {
+			if (ship.getMass() == originalMass){
+				ship.setMass(originalMass * 10f);
 			}
-			SystemDamageTakenBuffListener listener;
-			if (!ship.hasListenerOfClass(SystemDamageTakenBuffListener.class)) {
-				listener = new SystemDamageTakenBuffListener();
-				ship.addListener(listener);
-			} else {
-				listener = ship.getListeners(SystemDamageTakenBuffListener.class).get(0);
-			}
-			DAMAGE_MULT *= listener.GetBonusScale();
 		}
 
-		//stats.getShieldTurnRateMult().modifyMult(id, 1f);
-		//stats.getShieldUnfoldRateMult().modifyPercent(id, 2000);
-		
-		//stats.getShieldDamageTakenMult().modifyMult(id, 0.1f);
-		stats.getShieldDamageTakenMult().modifyPercent(id,  - DAMAGE_MULT * effectLevel);
+		float calculatedDamageMult = calculateDamageMult();
 
-		stats.getArmorDamageTakenMult().modifyPercent(id,  - DAMAGE_MULT * effectLevel);
-
-		stats.getHullDamageTakenMult().modifyPercent(id,  - DAMAGE_MULT * effectLevel);
-
-		//System.out.println("level: " + effectLevel);
-
+		stats.getShieldDamageTakenMult().modifyPercent(id, 1f - calculatedDamageMult);
+		stats.getArmorDamageTakenMult().modifyPercent(id, 1f - calculatedDamageMult);
+		stats.getHullDamageTakenMult().modifyPercent(id, 1f - calculatedDamageMult);
 	}
-	public class SystemDamageTakenBuffListener implements DamageTakenModifier, AdvanceableListener {
-
-		//data class to hold some info
-		public static class BuffData {
-			float damage;
-			float lifetime;
-
-			public BuffData(float damage, float lifetime) {
-				this.damage = damage;
-				this.lifetime = lifetime;
-			}
-		}
-
-		public ArrayList<BuffData> buffData = new ArrayList<>();
-
-		//bonus scale is 0 below this value
-		public static final float DamageForMinBonus = 200f;
-		//bonus scales up to 1 at this value
-		public static final float DamageforMaxBonus = 8000f;
+	public static class SystemDamageTakenBuffListener implements DamageTakenModifier, AdvanceableListener {
+		private float damageTaken = 0f;
+		private int hitsTaken = 0;
 
 		@Override
 		public void advance(float amount) {
-
-			//go over every buff and decrement their remaining lifetime
-			for (BuffData buff : new ArrayList<>(buffData)) {
-				buff.lifetime -= amount;
-			}
-
-			//fancy removal code
-			buffData.removeIf(buffData -> buffData.lifetime < 0);
 		}
 
 		@Override
 		public String modifyDamageTaken(Object param, CombatEntityAPI target, DamageAPI damage, Vector2f point, boolean shieldHit) {
 			//add a new buff data instance to the list
 			//multiplies by DPS duration so that beams don't do wacky numbers
-			buffData.add(new BuffData((damage.getDamage() + 200f) * (param instanceof BeamAPI ? damage.getDpsDuration():1), 5));
+			float dmg = (damage.getDamage()) * (param instanceof BeamAPI ? damage.getDpsDuration() : 1);
+
+			damageTaken += dmg;
+			hitsTaken++;
+
 			return "";
 		}
 
-		public float GetBonusScale() {
+		public void resetValues() {
+			damageTaken = 0f;
+		}
 
-			float damTaken = DamageforMaxBonus;
-			for (BuffData buff : buffData) {
-				damTaken += buff.damage;
-			}
+		public float GetBonusMult() {
+			float damageMult = Math.min((int) damageTaken / (int) DAMAGE_BONUS_STEP, (int) DAMAGE_FOR_MAX_BONUS / (int) DAMAGE_BONUS_STEP ) * DAMAGE_BONUS_EXTRA_MULT * BONUS_MULT_STEP;
+			float hitsMult = hitsTaken * BONUS_MULT_STEP * HIT_BONUS_EXTRA_MULT;
 
-			//if less than min damage, return 0
-			if (damTaken < DamageForMinBonus) {
-				return  0;
-			}
-
-			//if greater than max, return 1
-			if (damTaken > DamageforMaxBonus) {
-				return 1;
-			}
-
-			return (damTaken - DamageForMinBonus) / (DamageforMaxBonus - DamageForMinBonus);
+			return damageMult + hitsMult;
 		}
 	}
 	
@@ -126,12 +101,21 @@ public class TemporalBarrierStats extends BaseShipSystemScript {
 		if(ship == null){
 			return;
 		}
-		if(mass == null){
-			mass = ship.getMass();
+
+		if(originalMass == null){
+			originalMass = ship.getMass();
 		}
-		if(ship.getMass() != mass){
-			ship.setMass(mass);
+
+		if(ship.getMass() != originalMass){
+			ship.setMass(originalMass);
 		}
+
+		if(listener != null){
+			listener.resetValues();
+			lastActivationTime = 0f;
+		}
+
+		previousState = State.IDLE;
 		//stats.getShieldAbsorptionMult().unmodify(id);
 		stats.getShieldArcBonus().unmodify(id);
 		stats.getShieldDamageTakenMult().unmodify(id);
@@ -142,15 +126,30 @@ public class TemporalBarrierStats extends BaseShipSystemScript {
 		stats.getArmorDamageTakenMult().unmodify(id);
 		stats.getShieldUpkeepMult().unmodify(id);
 	}
-	
-	public StatusData getStatusData(int index, State state, float effectLevel) {
-		if (index == 0) {
-			return new StatusData("current damage reduction: " + DAMAGE_MULT, false);
-		}
-		if (index == 2) {
-			return new StatusData("Current mass: " + mass, false);
-		}
-			return null;
+
+	public float calculateDamageMult() {
+		float bonusMult = 0f;
+		if(listener != null){
+			bonusMult = listener.GetBonusMult();
 		}
 
+		float timeElapsed = Global.getCombatEngine().getTotalElapsedTime(false) - lastActivationTime;
+
+		float timeMult = Math.max((DURATION - timeElapsed) / DURATION, 0);
+
+		return Math.min(DAMAGE_MULT * timeMult + bonusMult, 99f);
 	}
+
+	public StatusData getStatusData(int index, State state, float effectLevel) {
+		if (index == 0) {
+			return new StatusData("current damage reduction: " + calculateDamageMult(), false);
+		}
+
+		if (index == 2) {
+			return new StatusData("Current mass: " + originalMass * 10f, false);
+		}
+
+		return null;
+	}
+
+}
